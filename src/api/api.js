420d9f7644201ff79681921a395e02b5e35dc2ab
Khess S Dotto <khessdotto@gmail.com>
2023-03-07 14:30:58 -0300

    Deleted 'ProtectedRoute' and created ProtectedRoutes for protectedClientRoute and protectedBusinessRoute.

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiURLs = {
-  development: "http://localhost:4000",
-  production: "LINK DA SUA API DEPLOYADA AQUI!",
+  development: "http://localhost:5000",
+  production: "https://fair-share.cyclic.app",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
