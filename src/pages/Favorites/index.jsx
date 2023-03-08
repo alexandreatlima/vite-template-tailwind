1b8ec2bc212efe86f56ee766245af0b00a6cd12e
Khess S Dotto <khessdotto@gmail.com>
2023-03-08 17:54:40 -0300

    Fixed favorites

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -9,8 +9,8 @@ export function Favorites() {
     async function fetchFavorites() {
       try {
         const response = await api.get("/api/user/get/all-favorites");
-        console.log(response.data);
-        setFavorites(response.data);
+        console.log(response.data.favorites);
+        setFavorites(response.data.favorites);
       } catch (err) {
         console.log(err);
       }
@@ -28,8 +28,8 @@ export function Favorites() {
         {favorites.map((currentFavorite) => {
           return (
             <article>
-              <img src={correntFavorite.picture} />
-              <h2>{currentFavorite.idUser}</h2>
+              <img src={currentFavorite.picture} />
+              <h2>{currentFavorite.name}</h2>
             </article>
           );
         })}
